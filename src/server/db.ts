18aab251eedc935f043496ee7230abf5da71952b
rahmsc <danmilad.code@gmail.com>
2024-08-29 07:44:55 +0200

    Created DB using Turso/Prisma for the first time Email for newsletter capture

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,17 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
-import { env } from "~/env";
+const libsql = createClient({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+});
 
-const createPrismaClient = () =>
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
-};
-
-export const db = globalForPrisma.prisma ?? createPrismaClient();
-
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+const adapter = new PrismaLibSQL(libsql);
+export const prisma = new PrismaClient({ adapter });
